ba01c1f8ecb0ef2d565af743622069eddb965c43
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2025-01-07 14:58:09 -0500

    Add quick button to move single snake square horizontally

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,23 +1,38 @@
 import { useState, useEffect } from 'react';
 
-const Board = ({ width = 10, height = 10 }) => {
+const Board = ({ width = 15, height = 15 }) => {
     const [matrix, setMatrix] = useState<any[]>([]);
+    const [snake, setSnake] = useState([3, 4]);
+
+    const drawSnake = () => {
+        let [x, y] = snake;
+        let updatedMatrix = [...matrix];
+        updatedMatrix[x][y] = 'E';
+        updatedMatrix[x + 1][y] = 'S'
+        setSnake([x + 1, y]);
+        setMatrix(updatedMatrix);
+    }
 
     useEffect(() => {
-        const filled = Array.from(Array(height).fill(null),
-            (_) => Array(width).fill('E'));
-        filled[2][4] = "S";
+        const filled = Array.from(Array(width).fill(null),
+            (_) => Array(height).fill('E'));
+        let [x, y] = snake;
+        filled[x][y] = "S";
         setMatrix(filled);
     }, []);
+
     return (
-        <div className="board">
-            {matrix.map(row =>
-                <div className="row">
-                    {row.map((square: String) =>
-                        <div className="square">{square}</div>
-                    )}
-                </div>)}
-        </div>
+        <>
+            <button onClick={drawSnake}>Move</button>
+            <div className="board">
+                {matrix.map(row =>
+                    <div className="row">
+                        {row.map((square: String) =>
+                            <div className={square === 'S' ? "snake" : 'square'}>{square}</div>
+                        )}
+                    </div>)}
+            </div>
+        </>
     )
 }
 
