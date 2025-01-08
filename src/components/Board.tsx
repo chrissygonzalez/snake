24ea7d2e1995e8316fae352897535820eaae6ae5
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2025-01-08 10:44:47 -0500

    Give snake a little length, add moveSnake fn and move whole snake on btn click

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,29 +1,60 @@
 import { useState, useEffect } from 'react';
 
+const SNAKE_START_LENGTH = 4;
+const SNAKE_START_DIRECTION = 'UP';
+
+const initializeBoard = (width: number, height: number): [any[][], number[][], string[]] => {
+    const filled = Array.from(Array(width).fill(null),
+        (_) => Array(height).fill(null));
+
+    const midPtX = Math.floor(width / 2);
+    const midPtY = Math.floor(height / 2);
+    const snakeSquares = [[midPtX, midPtY], [midPtX, midPtY + 1], [midPtX, midPtY + 2], [midPtX, midPtY + 3]];
+    const snakeStrings = [`${midPtX}-${midPtY}`, `${midPtX}-${midPtY + 1}`, `${midPtX}-${midPtY + 2}`, `${midPtX}-${midPtY + 3}`];
+    return [filled, snakeSquares, snakeStrings];
+}
+
 const Board = ({ width = 15, height = 15 }) => {
-    const [matrix, setMatrix] = useState<any[]>([]);
-    const [snake, setSnake] = useState([3, 4]);
+    const [filled, snakeSquares, snakeStrings] = initializeBoard(width, height);
+    const [matrix, setMatrix] = useState<any[]>(filled);
+    const [snakeArr, setSnakeArr] = useState(snakeSquares);
+    const [snakeSet, setSnakeSet] = useState(new Set([...snakeStrings]));
+
+    const moveSnake = () => {
+        let updatedSnake: number[][] = [...snakeArr];
+        const [headX, headY] = updatedSnake[0];
+        const newHead = [headX, headY - 1]
+        updatedSnake.unshift(newHead);
+        const [tailX, tailY] = updatedSnake.pop() || [];
+        setSnakeArr(updatedSnake);
+
+        let updatedSnakeSet = new Set([...snakeSet]);
+        updatedSnakeSet.delete(`${tailX}-${tailY}`);
+        updatedSnakeSet.add(`${newHead[0]}-${newHead[1]}`);
+        setSnakeSet(new Set(updatedSnakeSet));
+    }
 
     const drawSnake = () => {
-        let [x, y] = snake;
         let updatedMatrix = [...matrix];
-        updatedMatrix[x][y] = 'E';
-        updatedMatrix[x + 1][y] = 'S'
-        setSnake([x + 1, y]);
+        for (let i = 0; i < updatedMatrix.length; i++) {
+            for (let j = 0; j < updatedMatrix[0].length; j++) {
+                if (snakeSet.has(`${i}-${j}`)) {
+                    updatedMatrix[i][j] = 'S';
+                } else {
+                    updatedMatrix[i][j] = null;
+                }
+            }
+        }
         setMatrix(updatedMatrix);
     }
 
     useEffect(() => {
-        const filled = Array.from(Array(width).fill(null),
-            (_) => Array(height).fill('E'));
-        let [x, y] = snake;
-        filled[x][y] = "S";
-        setMatrix(filled);
-    }, []);
+        drawSnake();
+    }, [snakeSet]);
 
     return (
         <>
-            <button onClick={drawSnake}>Move</button>
+            <button onClick={moveSnake}>Move</button>
             <div className="board">
                 {matrix.map(row =>
                     <div className="row">
