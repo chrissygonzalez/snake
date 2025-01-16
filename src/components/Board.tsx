3607ffe5c7217431924cac243cab32333ea35265
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2025-01-16 14:46:52 -0500

    Try adding food to the grid

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -20,20 +20,45 @@ const getNextPosition = (x: number, y: number) => {
 }
 
 const Board = ({ initMatrix, initSnakeArr, initSnakeSet }: { initMatrix: any[][], initSnakeArr: any[], initSnakeSet: Set<string> }) => {
+    const ROW_LENGTH = initMatrix.length;
+    const COL_LENGTH = initMatrix[0].length;
     const [isPlaying, setIsPlaying] = useState(false);
     const [isLose, setIsLose] = useState(false);
     const [matrix, setMatrix] = useState<any[]>(initMatrix);
+    let food = [0, 0];
+    let foodCount = 0;
     let snakeArr = initSnakeArr;
     let snakeSet = initSnakeSet;
 
     const isWall = (x: number, y: number): boolean => {
         if (x < 0 || y < 0) return true;
-        if (x >= matrix[0].length || y >= matrix.length) return true;
+        if (x >= COL_LENGTH || y >= ROW_LENGTH) return true;
         return false;
     }
 
     const isSnake = (x: number, y: number): boolean => {
         return snakeSet.has(`${x}-${y}`);
+    }
+
+    const isFood = (x: number, y: number): boolean => {
+        return x === food[0] && y === food[1];
+    }
+
+    const getFoodPosition = () => {
+        // TODO: randomize foodCount and make longer
+        // maybe add a gap when food is not showing
+        if (foodCount >= 20) {
+            foodCount = 0;
+            let randomX = -1;
+            let randomY = -1;
+            while (isWall(randomX, randomY) || isSnake(randomX, randomY)) {
+                randomX = Math.floor(Math.random() * (COL_LENGTH - 1));
+                randomY = Math.floor(Math.random() * (ROW_LENGTH - 1));
+            }
+            food = [randomX, randomY];
+        } else {
+            foodCount++;
+        }
     }
 
     const updateSnakePosition = () => {
@@ -46,6 +71,7 @@ const Board = ({ initMatrix, initSnakeArr, initSnakeSet }: { initMatrix: any[][]
             setIsLose(true);
             return;
         }
+        // TODO: add branch for if isFood
         updatedSnake.unshift([newX, newY]);
         const [tailX, tailY] = updatedSnake.pop() || [];
         snakeArr = updatedSnake;
@@ -59,12 +85,15 @@ const Board = ({ initMatrix, initSnakeArr, initSnakeSet }: { initMatrix: any[][]
     const updateMatrix = () => {
         // console.log("updating matrix");
         updateSnakePosition();
+        getFoodPosition();
         setMatrix(matrix => {
             let updatedMatrix = [...matrix];
             for (let i = 0; i < updatedMatrix.length; i++) {
                 for (let j = 0; j < updatedMatrix[0].length; j++) {
                     if (snakeSet.has(`${i}-${j}`)) {
                         updatedMatrix[i][j] = 'S';
+                    } else if (isFood(i, j)) {
+                        updatedMatrix[i][j] = 'F';
                     } else {
                         updatedMatrix[i][j] = null;
                     }
@@ -99,6 +128,12 @@ const Board = ({ initMatrix, initSnakeArr, initSnakeSet }: { initMatrix: any[][]
         return () => document.removeEventListener('keydown', handleKeyDown);
     }, []);
 
+    // const getSquareClass = (content: string) => {
+    //     if (content === 'S') return 'snake';
+    //     if (content === 'F') return 'food';
+    //     return 'square';
+    // }
+
     return (
         <>
             {isLose && <div className="game-lost">You lose!</div>}
@@ -106,7 +141,9 @@ const Board = ({ initMatrix, initSnakeArr, initSnakeSet }: { initMatrix: any[][]
                 {matrix.map((row, rowIndex) =>
                     <div key={rowIndex} className="row">
                         {row.map((square: string, squareIndex: number) =>
-                            <div key={`${rowIndex}-${squareIndex}`} className={square === 'S' ? "snake" : 'square'}>
+                            <div
+                                key={`${rowIndex}-${squareIndex}`}
+                                className={['square', square === 'S' && 'snake', square === 'F' && 'food'].filter(Boolean).join(" ")}>
                             </div>
                         )}
                     </div>)}
