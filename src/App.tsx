a362a850ac5c95c40b9892019b43586ddfb81289
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2025-01-30 16:08:26 -0500

    Move board init back into Board, fix reset fn

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,10 @@
-import { useState } from 'react';
-import Board from './components/Board';
 import './App.css';
-
-const COLUMNS = 35;
-const ROWS = 35;
-const SNAKE_LENGTH = 6;
-
-const initializeBoard = (): { grid: any[][], snakeSquares: number[][], snakeSet: Set<string> } => {
-  // console.log("calling init");
-  const grid = Array.from(Array(COLUMNS).fill(null),
-    (_) => Array(ROWS).fill(null));
-  const midPtX = Math.floor(COLUMNS / 2);
-  const midPtY = Math.floor(ROWS / 2);
-  const snakeSquares = Array.from(Array(SNAKE_LENGTH), (_, index) => [midPtX, midPtY + index]);
-  const snakeSet = new Set(snakeSquares.map(s => `${s[0]}-${s[1]}`));
-  for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[0].length; j++) {
-      if (snakeSet.has(`${i}-${j}`)) {
-        grid[i][j] = 'S';
-      } else {
-        grid[i][j] = null;
-      }
-    }
-  }
-  return { grid, snakeSquares, snakeSet };
-}
+import Board from './components/Board';
 
 function App() {
-  const [initialState] = useState(initializeBoard);
-  const { grid, snakeSquares, snakeSet } = initialState;
-
   return (
     <div>
-      <Board initMatrix={grid} initSnakeArr={snakeSquares} initSnakeSet={snakeSet} />
+      <Board />
     </div>
   )
 }
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,6 +1,20 @@
 import { useState, useEffect, useRef } from 'react';
 import audioUrl from '../assets/test-trimmed.mp3';
 
+const COLUMNS = 35;
+const ROWS = 35;
+const SNAKE_LENGTH = 6;
+
+const getSnakeStartPosition = () => {
+    const midPtX = Math.floor(COLUMNS / 2);
+    const midPtY = Math.floor(ROWS / 2);
+    return Array.from(Array(SNAKE_LENGTH), (_, index) => [midPtX, midPtY + index]);
+}
+
+const getSnakeSet = (snakeArr: number[][]) => {
+    return new Set(snakeArr.map(s => `${s[0]}-${s[1]}`));
+}
+
 const SNAKE_SPEED = 100;
 let snakeDirection = 'UP';
 
@@ -20,23 +34,37 @@ const getNextPosition = (x: number, y: number) => {
     return [x, y];
 }
 
-const Board = ({ initMatrix, initSnakeArr, initSnakeSet }: { initMatrix: any[][], initSnakeArr: any[], initSnakeSet: Set<string> }) => {
+const Board = () => {
+    let snakeArr = getSnakeStartPosition();
+    let snakeSet = getSnakeSet(snakeArr);
+    const initializeBoard = () => {
+        const board = Array.from(Array(COLUMNS).fill(null),
+            (_) => Array(ROWS).fill(null));
+
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board[0].length; j++) {
+                if (snakeSet.has(`${i}-${j}`)) {
+                    board[i][j] = 'S';
+                } else {
+                    board[i][j] = null;
+                }
+            }
+        }
+        return board;
+    }
+
     const [isPlaying, setIsPlaying] = useState(false);
-    const [isGameLost, setIsGameLost] = useState(false);
-    const [matrix, setMatrix] = useState<any[]>(initMatrix);
+    const [isLoser, setIsLoser] = useState(false);
+    const [matrix, setMatrix] = useState<any[][]>(initializeBoard);
     const ref = useRef<HTMLAudioElement | null>(null);
 
-    const ROW_LENGTH = initMatrix.length;
-    const COL_LENGTH = initMatrix[0].length;
     let food = [0, 0];
     let foodCount = 0;
     let showFood = false;
-    let snakeArr = [...initSnakeArr];
-    let snakeSet = new Set(initSnakeSet);
 
     const isWall = (x: number, y: number): boolean => {
         if (x < 0 || y < 0) return true;
-        if (x >= COL_LENGTH || y >= ROW_LENGTH) return true;
+        if (x >= COLUMNS || y >= ROWS) return true;
         return false;
     }
 
@@ -58,8 +86,8 @@ const Board = ({ initMatrix, initSnakeArr, initSnakeSet }: { initMatrix: any[][]
             let randomX = -1;
             let randomY = -1;
             while (isWall(randomX, randomY) || isSnake(randomX, randomY)) {
-                randomX = Math.floor(Math.random() * (COL_LENGTH - 1));
-                randomY = Math.floor(Math.random() * (ROW_LENGTH - 1));
+                randomX = Math.floor(Math.random() * (COLUMNS - 1));
+                randomY = Math.floor(Math.random() * (ROWS - 1));
             }
             food = [randomX, randomY];
         } else if (foodCount < 30) {
@@ -81,7 +109,7 @@ const Board = ({ initMatrix, initSnakeArr, initSnakeSet }: { initMatrix: any[][]
         }
         if (isWall(newX, newY) || isSnake(newX, newY)) {
             setIsPlaying(false);
-            setIsGameLost(true);
+            setIsLoser(true);
             return;
         }
         moveSnake(newX, newY);
@@ -104,7 +132,6 @@ const Board = ({ initMatrix, initSnakeArr, initSnakeSet }: { initMatrix: any[][]
     const updateMatrix = () => {
         updateSnakePosition();
         updateFoodPosition();
-        // setMatrix(constructNextBoardState());
         setMatrix(matrix => {
             let updatedMatrix = [...matrix];
             for (let i = 0; i < updatedMatrix.length; i++) {
@@ -123,20 +150,15 @@ const Board = ({ initMatrix, initSnakeArr, initSnakeSet }: { initMatrix: any[][]
     };
 
     const handleResetGame = () => {
-        resetFood();
-        setIsGameLost(false);
-        foodCount = 0;
-        showFood = false;
-        snakeArr = initSnakeArr;
-        snakeSet = initSnakeSet;
-        setMatrix(initMatrix);
-        // TODO: don't move the snake one square when resetting
-        updateMatrix();
+        setIsLoser(false);
+        setMatrix(initializeBoard);
     }
 
     // TODO: debounce so you can't die as easily?
     const handleKeyDown = (e: KeyboardEvent) => {
-        if (!isPlaying) setIsPlaying(true);
+        if (!isPlaying) {
+            setIsPlaying(true);
+        }
         if (snakeDirection !== 'UP' && snakeDirection !== 'DOWN') {
             if (e.key === "ArrowUp") snakeDirection = 'UP';
             if (e.key === "ArrowDown") snakeDirection = 'DOWN';
@@ -160,12 +182,14 @@ const Board = ({ initMatrix, initSnakeArr, initSnakeSet }: { initMatrix: any[][]
 
     useEffect(() => {
         document.addEventListener('keydown', handleKeyDown);
+        if (isLoser) document.removeEventListener('keydown', handleKeyDown);
+
         return () => document.removeEventListener('keydown', handleKeyDown);
-    }, []);
+    }, [isLoser]);
 
     return (
         <div className="container">
-            {isGameLost && <div className="message-lose"><p>You lose!</p></div>}
+            {isLoser && !isPlaying && <div className="message-lose"><p>You lose!</p></div>}
             <div className="board">
                 {matrix.map((row, rowIndex) =>
                     <div key={rowIndex} className="row">
@@ -178,7 +202,7 @@ const Board = ({ initMatrix, initSnakeArr, initSnakeSet }: { initMatrix: any[][]
                     </div>)}
             </div>
             <audio ref={ref} loop={true} src={audioUrl} typeof='audio/mpeg' />
-            {isGameLost ? <button onClick={handleResetGame}>Reset</button> : <p>Hit any key to start</p>}
+            {isLoser && !isPlaying ? <button onClick={handleResetGame}>Reset</button> : <p>Hit any key to start</p>}
         </div>
     )
 }
